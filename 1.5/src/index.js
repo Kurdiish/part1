6718f25605f6fd1fc0be3b5bb0d6ff8868b6468e
Arash Niroumand Bigzadeh <arash.a.niroumandbigzadeh@utu.fi>
2021-02-23 01:39:10 +0200

    part1

--- 1.5/src/index.js
+++ 1.5/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
     return (
         <div>
-            <h1>{props.title}</h1>
+            <h1>{props.phonebookapp}</h1>
         </div>
     )
 }
